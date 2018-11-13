947e4e7538fac536a5eebee057082ed2e2aee6d1
Turrflurr <45006366+Turrflurr@users.noreply.github.com>
2018-11-13 14:17:51 +0000

    Update flask frame.py

--- bookingSite/flask frame.py
+++ bookingSite/flask frame.py
@@ -11,51 +11,13 @@ def home():
 def attractions():
 	return render_template('attactions.html')
 		
-@app.route('/contacts')
-def contacts():
-	skillList = readFile('static\\contacts.csv')
-	return render_template('contacts.html',skillList = skillList)
+@app.route('/rental')
+def rental():
+	return render_template('rental.html')
 
-def readFile(aFile):
-	with open(aFile,'r') as inFile:
-		reader = csv.reader(inFile)
-		skillList = [row for row in reader]
-	return skillList
-
-def writeFile(aList,aFile):
-	with open(aFile,'w',newline="") as outFile:
-		writer = csv.writer(outFile)
-		writer.writerows(aList)
-	return
-
-@app.route('/addSkill', methods = ['POST'])
-def addSkill():
-	skillFile = 'static\\skills.csv'
-	skillList = readFile(skillFile)
-	
-	skill = request.form['skill']
-	rating = request.form['rating']
-	
-	newSkill = [skill,rating]
-	skillList.append(newSkill)
-	
-	writeFile(skillList,skillFile)
-	return render_template('skills.html',skillList = skillList)
-
-@app.route('/addContact',methods = ['POST'])
-def addContact():
-	contactFile = 'static\\contacts.csv'
-	skillList = readFile(contactFile)
-	
-	contact = request.form['contact']
-	number = request.form['number']
-	
-	newContact = [contact, number]
-	skillList.append(newContact)
-	
-	writeFile(skillList,contactFile)
-	return render_template('contacts.html',skillList = skillList)
-
+@app.route('/reviews')
+def reviews():
+	return render_template('reviews.html')
 
 
 if __name__ == '__main__':
