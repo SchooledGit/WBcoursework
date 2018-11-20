edd6f822c248f141c47aba0835d9e317fb33693e
Turrflurr <45006366+Turrflurr@users.noreply.github.com>
2018-11-20 14:18:49 +0000

    Add files via upload

--- bookingSite/main.py
+++ bookingSite/main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from flask import request
 import csv
+import datetime
 app = Flask(__name__)
 
 @app.route('/')
@@ -11,13 +12,52 @@ def home():
 def attractions():
 	return render_template('attactions.html')
 		
-@app.route('/rental')
-def rental():
-	return render_template('rental.html')
-
 @app.route('/reviews')
 def reviews():
-	return render_template('reviews.html')
+	inList = readFile('static\\reviews.csv')
+	return render_template('reviews.html',inList = inList)
+
+def readFile(aFile):
+	with open(aFile,'r') as inFile:
+		reader = csv.reader(inFile)
+		inList = [row for row in reader]
+	return inList
+
+def writeFile(aList,aFile):
+	with open(aFile,'w',newline="") as outFile:
+		writer = csv.writer(outFile)
+		writer.writerows(aList)
+	return
+
+@app.route('/addReview', methods = ['POST'])
+def addReview():
+    reviewFile = 'static\\reviews.csv'
+    inList = readFile(reviewFile)
+	
+    name = request.form['name']
+    date = datetime.datetime.today().strftime('%Y-%m-%d')
+    rating = request.form['rating']
+    comment = request.form['comment']
+    newReview = [name,date,rating,comment]
+    inList.append(newReview)
+    
+    writeFile(inList,reviewFile)
+    return render_template('reviews.html',inList = inList)
+
+@app.route('/addContact',methods = ['POST'])
+def addContact():
+	contactFile = 'static\\contacts.csv'
+	skillList = readFile(contactFile)
+	
+	contact = request.form['contact']
+	number = request.form['number']
+	
+	newContact = [contact, number]
+	skillList.append(newContact)
+	
+	writeFile(skillList,contactFile)
+	return render_template('contacts.html',skillList = skillList)
+
 
 
 if __name__ == '__main__':
