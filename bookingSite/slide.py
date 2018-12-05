b8586f3f188786c651c5dd946ad722ad1fbb8db0
SchooledGit <45006335+SchooledGit@users.noreply.github.com>
2018-12-05 12:57:37 +0000

    Add files via upload

---
+++ bookingSite/slide.py
@@ -0,0 +1,11 @@
+class Slide:
+    source = ''
+    index = 0
+    reference = ''
+    caption = ''
+
+    def __init__(self, src, indx, ref, cptn):
+        self.source = src
+        self.index = indx
+        self.reference = ref
+        self.caption = cptn
